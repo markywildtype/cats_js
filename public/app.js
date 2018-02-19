0bfc50d2495491610a404fc902ae55f00592e0af
Mark Blanford <markytorture@hotmail.com>
2018-02-19 18:43:03 +0000

    second commit

--- public/app.js
+++ public/app.js
@@ -1,12 +1,27 @@
-const app = function(name, food, image){
+const addCat = function(name, food, image){
 
-  const name = document.createElement('li');
-  name.innerText = name;
+  const cat = document.createElement('ul');
 
-  const food = document.createElement('li')
-  food.innerText = food;
+  const catName = document.createElement('li');
+  catName.innerText = "Salem";
 
+  const favFood = document.createElement('li');
+  favFood.innerText = "Lizard Flakes";
+
+  const catImage = document.createElement('li');
+  catImage.innerHTML = '<img src ="http://bit.ly/2FbWSex" width="500" />';
+
+  const catSection = document.getElementById('cats')
+  cat.appendChild(catName);
+  cat.appendChild(favFood);
+  cat.appendChild(catImage);
+  catSection.appendChild(cat);
+
+  console.log("I am loading!");
 }
 
+const app = function(){
+  addCat()//"Salem", "Lizard Flakes", "https://www.n3rdabl3.com/wp-content/images/uploads/2018/02/salem3EDIT.jpg")
+}
 
 window.document.addEventListener("DOMContentLoaded", app);
