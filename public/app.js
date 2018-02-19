ae2a54d858f62f0c2a63c657940aa923f4b63f3a
Mark Blanford <markytorture@hotmail.com>
2018-02-19 17:47:47 +0000

    initial commit

---
+++ public/app.js
@@ -0,0 +1,12 @@
+const app = function(name, food, image){
+
+  const name = document.createElement('li');
+  name.innerText = name;
+
+  const food = document.createElement('li')
+  food.innerText = food;
+
+}
+
+
+window.document.addEventListener("DOMContentLoaded", app);
