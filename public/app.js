d28a93ecc6ab6cf2381b61a543cee4c82622359b
Mark Blanford <markytorture@hotmail.com>
2018-02-19 18:51:11 +0000

    added cat array to populate the page

--- public/app.js
+++ public/app.js
@@ -1,15 +1,19 @@
-const addCat = function(name, food, image){
+// const addCat = function(name, food, image){
+const addCat = function(params){
 
   const cat = document.createElement('ul');
 
   const catName = document.createElement('li');
-  catName.innerText = name;
+  // catName.innerText = name;
+  catName.innerText = params.name;
 
   const favFood = document.createElement('li');
-  favFood.innerText = food;
+  // favFood.innerText = food;
+  favFood.innerText = params.food;
 
   const catImage = document.createElement('li');
-  catImage.innerHTML = '<img src ="' + image + '" width="500" />';
+  // catImage.innerHTML = '<img src ="' + image + '" width="500" />';
+  catImage.innerHTML = '<img src ="' + params.image + '" width="500" />';
 
   const catSection = document.getElementById('cats')
   cat.appendChild(catName);
@@ -20,11 +24,21 @@ const addCat = function(name, food, image){
   console.log("I am loading!");
 }
 
+const catArray = [
+  {name: "Boba", food: "Sock Fluff", image: "http://bit.ly/2riTB4W"},
+  {name: "Barnaby", food: "Tuna", image: "http://bit.ly/2CwgpTW"},
+  {name: "Max", food: "Whiskas Temptations", image: "http://bit.ly/2CwgpTW"},
+  {name: "Salem", food: "Lizard Flakes", image: "http://bit.ly/2FbWSex"}
+]
+
 const app = function(){
-  addCat("Boba", "Sock fluff", "http://bit.ly/2riTB4W");
-  addCat("Barnaby", "Tuna", "http://bit.ly/2CwgpTW");
-  addCat("Max", "Whiskas Temptations", "http://bit.ly/2qkvGoE");
-  addCat("Salem", "Lizard Flakes", "http://bit.ly/2FbWSex");
+  for (var i = 0; i < catArray.length; i++) {
+    addCat(catArray[i])
+  }
+  // addCat("Boba", "Sock fluff", "http://bit.ly/2riTB4W");
+  // addCat("Barnaby", "Tuna", "http://bit.ly/2CwgpTW");
+  // addCat("Max", "Whiskas Temptations", "http://bit.ly/2CwgpTW");
+  // addCat("Salem", "Lizard Flakes", "http://bit.ly/2FbWSex");
 }
 
 window.document.addEventListener("DOMContentLoaded", app);
