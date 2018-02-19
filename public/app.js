7ecbe6c452c6cb2ad32cbec7021923f2dc3e622c
Mark Blanford <markytorture@hotmail.com>
2018-02-19 19:36:00 +0000

    slight refactoring

--- public/app.js
+++ public/app.js
@@ -1,18 +1,22 @@
-// const addCat = function(name, food, image){
-const addCat = function(params){
+const catArray = [
+  {name: "Boba", food: "Sock Fluff", image: "http://bit.ly/2riTB4W"},
+  {name: "Barnaby", food: "Tuna", image: "http://bit.ly/2CwgpTW"},
+  {name: "Max", food: "Whiskas Temptations", image: "http://bit.ly/2qkvGoE"},
+  {name: "Salem", food: "Lizard Flakes", image: "http://bit.ly/2FbWSex"}
+]
 
+
+const addCat = function(params){
   const cat = document.createElement('ul');
+  cat.style.listStyleType = "none";
 
   const catName = document.createElement('li');
-  // catName.innerText = name;
   catName.innerText = params.name;
 
   const favFood = document.createElement('li');
-  // favFood.innerText = food;
   favFood.innerText = params.food;
 
   const catImage = document.createElement('li');
-  // catImage.innerHTML = '<img src ="' + image + '" width="500" />';
   catImage.innerHTML = '<img src ="' + params.image + '" width="500" />';
 
   const catSection = document.getElementById('cats')
@@ -20,25 +24,13 @@ const addCat = function(params){
   cat.appendChild(favFood);
   cat.appendChild(catImage);
   catSection.appendChild(cat);
-
-  console.log("I am loading!");
 }
 
-const catArray = [
-  {name: "Boba", food: "Sock Fluff", image: "http://bit.ly/2riTB4W"},
-  {name: "Barnaby", food: "Tuna", image: "http://bit.ly/2CwgpTW"},
-  {name: "Max", food: "Whiskas Temptations", image: "http://bit.ly/2CwgpTW"},
-  {name: "Salem", food: "Lizard Flakes", image: "http://bit.ly/2FbWSex"}
-]
 
 const app = function(){
   for (var i = 0; i < catArray.length; i++) {
     addCat(catArray[i])
   }
-  // addCat("Boba", "Sock fluff", "http://bit.ly/2riTB4W");
-  // addCat("Barnaby", "Tuna", "http://bit.ly/2CwgpTW");
-  // addCat("Max", "Whiskas Temptations", "http://bit.ly/2CwgpTW");
-  // addCat("Salem", "Lizard Flakes", "http://bit.ly/2FbWSex");
 }
 
 window.document.addEventListener("DOMContentLoaded", app);
